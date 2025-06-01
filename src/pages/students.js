7b13df17d85df9a2adc3161ec604651f2ac0a1c0
Achyuta K Upadya <achyuta0001@gmail.com>
2025-06-01 16:50:31 +0530

    feat/general page skeleton added

---
+++ src/pages/students.js
@@ -0,0 +1,9 @@
+const Students = () => {
+    return (
+        <div>
+        <h1>Students Page</h1>
+        <p>This is the students page where you can manage student information.</p>
+        </div>
+    );
+    }
+    export default Students;
